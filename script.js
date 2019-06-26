c3f02623b3ae0a64b8af64d7d3ac19a4ae1c9d81
Alex O'M <alex@Alex-OMs-MacBook-Pro.local>
2019-06-26 10:43:25 +0100

    Edited navbar - still needs dropdown to work

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-script
+// Navbar
+function responsiveNavBar() {
+  var x = document.getElementById("topnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+var prevScrollPos = window.pageYOffset
+window.onscroll = scrollNavBar() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollPos > currentScrollPos) {
+    document.getElementById("topnav").style.top = "0";
+  } else {
+    document.getElementById("topnav").style.top = "-50px";
+  }
+  prevScrollPos = currentScrollPos;
+}
+// Navbar ends
