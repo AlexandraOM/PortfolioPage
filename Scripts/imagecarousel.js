d803588e0cca20843b080622b81bc153f1d862a4
Alex O'M <alex@Alex-OMs-MacBook-Pro.local>
2019-07-02 13:25:16 +0100

    Changing editing js to shorter code, changing buttons to a tags, onclick for carousel functioning. Must now add automatic playing and ekey functionality

--- Scripts/imagecarousel.js
+++ Scripts/imagecarousel.js
@@ -19,57 +19,32 @@ document.addEventListener("keydown", function(event) {
     }
   }
 });
+var slideIndex = 1;
+showSlides(slideIndex);
 
-var slides = document.querySelectorAll("#slides .slide");
-var currentSlide = 0; // i.e. the slide index
-var slideInterval = setInterval(nextSlide, 2000);
-
-function nextSlide() {
-  goToSlide(currentSlide + 1);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
 
-function previousSlide() {
-  goToSlide(currentSlide - 1);
-}
-
-function goToSlide(n) {
-  slides[currentSlide].className = "slide";
-  currentSlide = (n + slides.length) % slides.length;
-  slides[currentSlide].className = "slide showing";
-}
-
-var playing = true;
-var pauseButton = document.getElementById("pause");
-
-function pauseSlideshow() {
-  pauseButton.innerHTML = "&#9658";
-  playing = false;
-  clearInterval(slideInterval);
-}
-
-function playSlideshow() {
-  pauseButton.innerHTML = "||";
-  playing = true;
-  slideInterval = setInterval(nextSlide, 2000);
-}
-
-pauseButton.onclick = function() {
-  if (playing) {
-    pauseSlideshow();
-  } else {
-    playSlideshow();
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
   }
-};
-
-var next = document.getElementById("next");
-var previous = document.getElementById("previous");
-
-next.onclick = function() {
-  pauseSlideshow();
-  nextSlide();
-};
-
-previous.onclick = function() {
-  pauseSlideshow();
-  previousSlide();
-};
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
