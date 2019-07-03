cc0325a03aa1945707f7d8548374dabc829a3248
Alex O'M <alex@Alex-OMs-MacBook-Pro.local>
2019-07-03 13:41:05 +0100

    The carousel works, FINALLY just need to make ekey functionality work

--- Scripts/imagecarousel.js
+++ Scripts/imagecarousel.js
@@ -21,57 +21,71 @@ document.addEventListener("keydown", function(event) {
 });
 
 // w3 schools //
+// this plays the slideshow automatically, starts at index 0 (i.e. first image)
+var playing = true;
+var slideIndex = 0;
+showSlides(1);
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function showSlides(n) {
+function loopSlides(nextOrPrev) {
   var i;
   var slides = document.getElementsByClassName("slide");
-  var dots = document.getElementsByClassName("demo");
+  var caption = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+
+  slides[slideIndex].style.display = "block";
+
+  captionText.innerHTML = caption[slideIndex].alt;
+
+  if (nextOrPrev === 1) {
+    slideIndex = slideIndex + 1;
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
+
+  if (nextOrPrev === -1) {
+    slideIndex = slideIndex - 1;
+  }
+
+  if (slideIndex + 1 > slides.length) {
+    slideIndex = 0;
+  }
+}
+
+function showSlides(nextOrPrev) {
+  if (playing) {
+    loopSlides(nextOrPrev);
+    setTimeout(function() {
+      showSlides(1);
+    }, 3000);
+  }
 }
 
 // https://codepen.io/dubhcait/pen/dwRPaJ //
-var playing = true;
+
 var pauseButton = document.getElementById("pause");
+// var slideInterval = setInterval(plusSlides(0), 3000);
 
 function pauseSlideshow() {
   pauseButton.innerHTML = "&#9658";
   playing = false;
-  clearInterval(slideInterval);
+  // clearInterval(slideInterval);
 }
 
 function playSlideshow() {
   pauseButton.innerHTML = "||";
   playing = true;
-  slideInterval = setInterval(nextSlide, 2000);
+  console.log("hello");
+  // slideInterval =
+  showSlides(1);
 }
 
-function pauseSlideshow() {
+function controlSlideshow() {
   if (playing) {
     pauseSlideshow();
   } else {
     playSlideshow();
   }
 }
+
+// pauseSlideshow();
