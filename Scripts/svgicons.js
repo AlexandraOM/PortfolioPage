dc255a1d59327877562d666096502353c1e68fab
Alex O'M <alex@Alex-OMs-MacBook-Pro.local>
2019-07-08 20:20:16 +0100

    Added font awesome library am now able toggle between the colours. Need to set size of icons now

--- Scripts/svgicons.js
+++ Scripts/svgicons.js
@@ -1,6 +1,14 @@
 // SVG Icons
 // function iconAnimation() {
 //   var x = document.getElementById("footerIcons");
-var linkImage = document.getElementsByClassName("footerIcons");
-linkImage.onhover.style.display = "none";
+var blackImage = document.getElementsByClassName("footerIcons blackIcon");
+var orangeImage = document.getElementsByClassName("footerIcons orangeIcon");
+function switchElement() {
+  blackImage.style.display = "none";
+  orangeImage.style.display = "active";
+}
+
+//
+// slides[slideIndex].style.display = "block";
 // }
+blackImage.addEventListener("mouseover", switchElement());
