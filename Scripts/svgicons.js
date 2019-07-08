eda5d32f18dd8cd6bff370a062bf2b0d246cd640
Alex O'M <alex@Alex-OMs-MacBook-Pro.local>
2019-07-08 15:54:25 +0100

    Have added CSS code to exhange SVGs for monotone to colour - still need to edit so the boxes don't continue to get bigger (add a limit)

--- Scripts/svgicons.js
+++ Scripts/svgicons.js
@@ -1,1 +1,6 @@
 // SVG Icons
+// function iconAnimation() {
+//   var x = document.getElementById("footerIcons");
+var linkImage = document.getElementsByClassName("footerIcons");
+linkImage.onhover.style.display = "none";
+// }
